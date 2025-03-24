9473c8643f4db21b948846b449574b87c695162c
Asaf Ninio <asafninio@gmail.com>
2025-03-24 15:51:18 +0200

    fix: remove unused searchParams variable

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,10 +2,8 @@
 
 import Header from '@/components/Header';
 import { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation';
 
 export default function SearchPage() {
-  const searchParams = useSearchParams();
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
